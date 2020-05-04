390e1a1f6ed1e94461e750f67625380baae2ad7e
Alisher Sadikov <alishersadikov@gmail.com>
2020-05-04 06:22:35 -0500

    Resistor color solution

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class ResistorColor
+{
+  public static int ColorCode(string color)
+  {
+    var colors = Colors();
+    if (!Array.Exists(colors, element => element == color))
+      throw new NotImplementedException(color + " is not a supported color");
+
+    return Array.IndexOf(colors, color);
+  }
+
+  public static string[] Colors()
+  {
+    return new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+  }
+}
